7719c3b4d877568a4b0e2464bcb72acb5710a1ad
Vesna Stojanovic <s.vesna96@gmail.com>
2022-02-16 22:39:16 +0100

    Model for Cinema, Actor, Producer, Movie added

---
+++ Data/MovieCategory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaTickets.Data
+{
+    public enum MovieCategory
+    {
+        Action, 
+        Comedy,
+        Triller,
+        Romance,
+        Drama,
+        Documentary
+    }
+}
