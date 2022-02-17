a6ab368957c14a473e93f527303b58c27d001d2e
Vesna Stojanovic <s.vesna96@gmail.com>
2022-02-17 01:42:07 +0100

    Model relationships added

--- Models/Actor.cs
+++ Models/Actor.cs
@@ -13,5 +13,8 @@ namespace CinemaTickets.Models
         public string ProfilePictureURL { get; set; }
         public string FullName { get; set; }
         public string Bio { get; set; }
+
+        //Relationship
+        public List<Actor_Movie> Actors_Movies { get; set; }
     }
 }
