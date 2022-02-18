c9739bdefb3ee475f7f1c080adbb2c7ad885a11f
Vesna Stojanovic <s.vesna96@gmail.com>
2022-02-18 22:41:32 +0100

    sql server config

--- Models/Actor_Movie.cs
+++ Models/Actor_Movie.cs
@@ -7,9 +7,9 @@ namespace CinemaTickets.Models
 {
     public class Actor_Movie
     {
-        public int MovieId { get; set; }
-        public int ActorId { get; set; }
         public Movie Movie { get; set; }
+        public int MovieId { get; set; }
         public Actor Actor { get; set; }
+        public int ActorId { get; set; }
     }
 }
