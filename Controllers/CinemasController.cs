a6a4a8ba46ba65e8ec67be016d0ddff54175426f
Vesna Stojanovic <s.vesna96@gmail.com>
2022-02-20 01:32:10 +0100

    initial controllers for actors, movies, cinemas and producers added

---
+++ Controllers/CinemasController.cs
@@ -0,0 +1,25 @@
+﻿using CinemaTickets.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CinemaTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allCinemas = await _context.Producers.ToListAsync();
+            return View();
+        }
+    }
+}
