fa2f2a783436f43e87de5f2427f22228096615cc
Vesna Stojanovic <s.vesna96@gmail.com>
2022-05-02 22:19:43 +0200

    ActorsService, IActorsService added

--- Startup.cs
+++ Startup.cs
@@ -1,4 +1,5 @@
 using CinemaTickets.Data;
+using CinemaTickets.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,6 +29,8 @@ namespace CinemaTickets
             //DbContext Configuration
             services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString
                 ("DefaultConnectionString")));
+
+            services.AddScoped<IActorsService, ActorsService>();
 
             services.AddControllersWithViews();
         }
